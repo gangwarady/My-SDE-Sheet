47a861226b4a264dfac29dc737ce4da0761a5935
ADITYA SINGH GANGWAR <103820233+gangwarady@users.noreply.github.com>
2022-07-19 13:02:57 +0530

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
+        if(list1->val<=list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
+        else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0){
+            return NULL;
+        }
+        while(lists.size()!=1){
+            ListNode* newnode = mergeTwoLists(lists[lists.size()-1], lists[lists.size()-2]);
+            lists.pop_back();
+            lists.pop_back();
+            lists.push_back(newnode);
+            
+        }
+        return lists[0];
+    }
+};

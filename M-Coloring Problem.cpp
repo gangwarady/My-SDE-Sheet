45f6d152325f19a000dfdf92ea70ef637124c4bc
ADITYA SINGH GANGWAR <103820233+gangwarady@users.noreply.github.com>
2022-09-14 15:59:50 +0530

    Create M-Coloring Problem.cpp

---
+++ M-Coloring Problem.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    bool check(int node, bool graph[101][101], int col, int n, vector<int> &color){
+        for(int i = 0 ; i<n;i++){
+            if(i!=node && graph[i][node]==1 && color[i]==col){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool solve(int node, bool graph[101][101], int m, int n, vector<int> &color){
+        if(node==n){
+            return true;
+        }
+        for(int i =1; i<=m; i++){
+            if(check(node,graph,i,n,color)){
+                color[node]=i;
+                if(solve(node+1, graph,m,n,color))
+                    return true;
+                color[node]=0; 
+            }
+        }
+        return false;
+    }
+    bool graphColoring(bool graph[101][101], int m, int n) {
+        // your code here
+        vector<int> color(n,0);
+        if(solve(0, graph,m,n,color))
+            return true;
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++) {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
